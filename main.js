ce3674b3f20f9fafb56844106aec74853cefa305
Zac Morton <zmorton8057@gmail.com>
2019-06-04 11:27:17 -0700

    Space Bar initializes the game

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var library = ["bookkeeper", "madonna", "vatican", 'zebra'];
 var randomLibrary
 var currentGuessWord = [];
 var randomLibrarySplit
+var guessCounter = 10;
 //Logs user input into a function
 var initGame = function () {
     currentGuessWord = []
@@ -12,30 +13,53 @@ var initGame = function () {
     randomLibrarySplit = randomLibrary.split("");
     for (var i = 0; i < randomLibrarySplit.length; i++) {
         currentGuessWord.push('_')
+        console.log(currentGuessWord)
+        console.log(randomLibrarySplit)
+        console.log(randomLibrary)
     }
     drawGameState()
+    if (randomLibrarySplit.includes(userGuess)) {
+    for (var j = 0; j < randomLibrarySplit.length; j++) {
+        if(userGuess===randomLibrarySplit[j]){
+            currentGuessWord.splice(j, 1, userGuess)
+        }
+            
+        }
+    }
 }
+
+
+//Joins blank array to print in the DOM
 var drawGameState = function () {
     document.getElementById('placeH').textContent = currentGuessWord.join(' ')
+    document.getElementById('guess').textContent = guessCounter
 }
+//Press Space Bar to Initialize game    
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        initGame()//your code
+    }
+}
+//Log User Key
 document.onkeyup = function (e) {
     var userGuess = e.key;
     console.log(userGuess)
 
-initGame()
+ 
+
+ 
+ 
 //check to see if user guessed correct letter
 //which checks against an array
 //if element at that index is equal to the userGuess return userGuess to that index
 if (randomLibrarySplit.includes(userGuess)) {
-    for (var j = 0; j < library.length; j++) {
-        if (userGuess === randomLibrarySplit[j]){
-            currentGuessWord.replaceChild(j, i)
+    for (var j = 0; j < randomLibrarySplit.length; j++) {
+        if(userGuess===randomLibrarySplit[j]){
+            currentGuessWord.splice(j, 1, userGuess)
+        } guessCounter--
+            
         }
     }
 }
-}
 
-console.log(library);
-console.log(currentGuessWord);
-console.log(randomLibrary);
-console.log(randomLibrarySplit);
+
